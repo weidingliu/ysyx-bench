565ba10574a561ecbd0e5f449dfd314731947797
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 21:35:22 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:35:22 up  2:06,  1 user,  load average: 0.89, 1.10, 1.09

---
+++ npc/playground/src/DoubleSwitch.scala
@@ -0,0 +1,15 @@
+import chisel3._
+
+/**
+ *DoubleSwitch IO define
+ */
+class doubleIO extends Bundle{
+  val a=Input(UInt(1.W))
+  val b=Input(UInt(1.W))
+  val out=Output(UInt(1.W))
+}
+
+class DoubleSwitch extends Module {
+    val io=IO(new doubleIO)//IO define
+    io.out := io.a & io.b
+}
--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,3 +1,9 @@
-object Elaborate extends App {
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new top())))
+import chisel3.stage.ChiselStage
+
+//package DoubleSwitch
+//object Elaborate extends App {
+//  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new top())))
+//}
+object Elaborate extends App{
+  (new ChiselStage).emitVerilog(new DoubleSwitch())
 }
