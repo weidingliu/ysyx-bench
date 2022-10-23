77ca5587d81c924df26d4f44452668fc55bf8b8d
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 17:38:18 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:38:18 up 8 min,  1 user,  load average: 3.16, 3.25, 1.78

--- npc/src/main/scala/Double_Switch/Spec.scala
+++ npc/src/main/scala/Double_Switch/Spec.scala
@@ -1,9 +1,12 @@
 package Double_Switch
 
-import chisel3._
 import chisel3.stage._
 //import java.sql.Driver
 
 object Spec extends App{
   (new ChiselStage).emitVerilog(new Double_Switch())
 }
+
+//object Spec extends App {
+//  Driver.execute(args, () => new Double_Switch)
+//}
