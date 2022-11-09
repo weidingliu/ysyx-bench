174e8609e9d6aa44b12024b6cdbfbf74d2734051
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-09 23:53:11 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:11 up  2:28,  1 user,  load average: 1.13, 0.75, 0.51

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -18,7 +18,7 @@ DIFF_REF_PATH = $(NEMU_HOME)/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-so
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
-
+$(warning $(GUEST_ISA))
 ifndef CONFIG_DIFFTEST_REF_NEMU
 $(DIFF_REF_SO):
 	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
