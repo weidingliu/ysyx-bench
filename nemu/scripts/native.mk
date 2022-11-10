29b96593f4296861b268b5e6695b66b083d2ea88
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 22:16:56 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:16:56 up  1:06,  1 user,  load average: 0.87, 0.56, 0.44

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -36,8 +36,6 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	-$(NEMU_EXEC)
-	#$(info $(call git_commit, "run NEMU"))
-	#$(info $(NEMU_EXEC))
 	
 
 gdb: run-env
