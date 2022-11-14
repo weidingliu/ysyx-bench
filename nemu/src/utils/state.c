0c384c6c54724e3dd4a33d6cf3153d99cebf49d4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 20:53:24 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:24 up 25 min,  1 user,  load average: 0.31, 0.50, 0.63

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -18,8 +18,9 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
+  /*int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  printf("%d",good);
-  return good;
+  //printf("%d",good);
+  */
+  return 0;
 }
