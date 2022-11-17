f5a654a54d8990197ff58efc6737e4299a95e56c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 10:14:38 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:14:38 up 13 min,  1 user,  load average: 1.00, 1.24, 1.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -102,7 +102,7 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
           case TK_NOTYPE:  break;
           case '+':{
-              strcpy(tokens[nr_token].str,rules[i].regex);
+              strcpy(tokens[nr_token].str,"+");
               tokens[nr_token++].type=rules[i].token_type;
               
               break;
@@ -134,7 +134,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
   for (int i=0;i<nr_token;i++){
-      printf("%s",tokens[i].str);
+      printf("%s ",tokens[i].str);
   }
   printf("\n");
   return 0;
