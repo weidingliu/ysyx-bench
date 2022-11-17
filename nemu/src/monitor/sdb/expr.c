f32730e69568ba3117c29b80895430653ff89d54
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 11:06:34 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:06:34 up  1:05,  1 user,  load average: 1.14, 1.44, 1.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -148,9 +148,8 @@ static bool make_token(char *e) {
                   printf("parameter is too long!!");
                   return 0;
               }
-              strcpy(tokens[nr_token].str,"0");
+              strncpy(tokens[nr_token].str,substr_start,substr_len);
               tokens[nr_token++].type=rules[i].token_type;
-              
               break;
               }
           
