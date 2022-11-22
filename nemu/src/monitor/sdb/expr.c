4b61d214d9a9112b0d953f16afc1fdd756155057
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 11:05:24 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:24 up 4 min,  1 user,  load average: 3.73, 2.62, 1.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -365,6 +365,8 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
+  
+  printf("%d",check_parentheses(0,nr_token-1));
   word_t out=evaluate(0,nr_token-1);
   printf("%ld",out);
   //check_parentheses(0,nr_token-1);
