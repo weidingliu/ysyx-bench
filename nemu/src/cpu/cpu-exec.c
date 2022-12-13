ce507e02972919578bcf798e6f76f6099daee0cc
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-13 17:42:29 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:29 up  2:19,  3 users,  load average: 0.78, 0.39, 0.34

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -17,7 +17,7 @@
 #include <cpu/decode.h>
 #include <cpu/difftest.h>
 #include <locale.h>
-
+#include </home/liuweiding/ysyx-workbench/nemu/src/monitor/sdb/sdb.h>
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -38,14 +38,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  #ifdef CONFIG_WATCHPOINT
+ // #ifdef CONFIG_WATCHPOINT
       bool flag=true;
       flag=check_watchpoint();
       printf("is here\n");
       if(!flag){
           nemu_state.state=NEMU_STOP;
       }
-  #endif
+  //#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
