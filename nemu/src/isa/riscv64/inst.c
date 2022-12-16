515ba0f0d9e8a57c3b846a88471e509bddeb7dc1
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-16 20:16:06 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:16:06 up  4:37,  1 user,  load average: 0.34, 0.25, 0.28

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = SEXT(BITS(i,31,31)<<19 + BITS(i,30,23)+BITS(i,22,22)<<8 + BITS(i,21,12)<<9,20) }while(0)
+#define immJ() do { *imm = SEXT(((BITS(i,31,31)<<19) | (BITS(i,30,23))|(BITS(i,22,22)<<8) | (BITS(i,21,12)<<9)),20); }while(0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -44,7 +44,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_J:                           break;
+    case TYPE_J:                   immJ(); break;
   }
 }
 
@@ -66,7 +66,7 @@ static int decode_exec(Decode *s) {
   
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui     , U, R(dest) = imm);
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi     , I, R(dest) = imm + src1);
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal     , U, R(dest) = s->pc+4;s->dnpc=s->pc+imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal     , J, R(dest) = s->pc+4;s->dnpc=s->pc+imm);
   
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
