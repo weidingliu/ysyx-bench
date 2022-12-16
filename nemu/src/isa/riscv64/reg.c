c34a4da0adca950733bcf8c35e1ebb6e432816d9
liuweidin <1045251744@qq.com>
2022-12-16 22:16:24 +0800

    On branch pa2  Changes to be committed: 	modified:   nemu/src/isa/riscv64/inst.c 	modified:   nemu/src/isa/riscv64/reg.c

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
     for (int i=0;i<32;i++){
-        printf("%s:%08x\n",regs[i],*regs[i]);
+        printf("%s:%08lx\n",regs[i],cpu.gpr[i]);
     }
     
 }
