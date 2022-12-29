444a6af69a3a384ea9ac5a96ead7ca85dea6c5c2
liuweidin <1045251744@qq.com>
2022-12-29 21:45:22 +0800

    On branch pa2  Changes to be committed: 	modified:   npc/src/main/scala/One_cycle/CPUTop.scala 	modified:   npc/src/main/scala/One_cycle/EXU.scala

--- npc/src/main/scala/One_cycle/CPUTop.scala
+++ npc/src/main/scala/One_cycle/CPUTop.scala
@@ -2,5 +2,32 @@ package One_cycle
 import chisel3._
 import chisel3.util._
 class CPUTop extends Module with paramete{
+  val io = IO(new Bundle() {
+    val pc=Output(UInt(xlen.W))
+    val inst=Input(UInt(instlen.W))
+  })
+  val IF=Module(new IFU)
 
+  val ID = Module(new IDU)
+
+  val EX = Module(new EXU)
+
+  val Reg = Module(new RF)
+
+  io.pc := IF.io.pc
+
+  ID.io.inst := io.inst
+
+  ID.io.ctrlIO <> EX.io
+
+  EX.io1.REG1 := Reg.read(ID.io.ctrlIO.src1)
+  EX.io1.REG2 := Reg.read(ID.io.ctrlIO.src2)
+
+  Reg.write(ID.io.ctrlIO.rd,EX.io1.result)
+}
+
+import chisel3.stage._
+
+object Spec extends App{
+  (new ChiselStage).emitVerilog(new CPUTop(),Array("--target-dir", "build"))
 }
--- npc/src/main/scala/One_cycle/EXU.scala
+++ npc/src/main/scala/One_cycle/EXU.scala
@@ -11,20 +11,39 @@ object ALUOPType{
 
 class EXU extends Module with paramete with InstrType {
   val io = IO(new Bundle() {
-    val exuIO = new IDUIO()
+//    val ctrlIO = new IDUIO
+    val src1type = Input(SRCType())
+    val src2type = Input(SRCType())
+    val src1 = Input(UInt(5.W))
+    val src2 = Input(UInt(5.W))
+
+    val rd = Input(UInt(5.W))
+    val futype = Input(FUType())
+    val aluoptype = Input(ALUOPType())
+    val Imm = Input(UInt(xlen.W))
+
+
+
+  })
+  val io1 = IO(new Bundle() {
     val REG1 = Input(UInt(xlen.W))
     val REG2 = Input(UInt(xlen.W))
+    val result = Output(UInt(xlen.W))
   })
-  val src1 = ListLookup(io.exuIO.src1type,List(0.U),Array(
-    BitPat(SRCType.R) -> List(io.REG1),
+  val src1 :: Nil= ListLookup(io.src1type,List(0.U(xlen.W)),Array(
+    BitPat(SRCType.R) -> List(io1.REG1),
 
   ))
-  val src2 = ListLookup(io.exuIO.src1type, List(0.U), Array(
-    BitPat(SRCType.R) -> List(io.REG2),
-    BitPat(SRCType.imm) -> List(io.exuIO.Imm)
+  val src2 :: Nil= ListLookup(io.src2type, List(0.U(xlen.W)), Array(
+    BitPat(SRCType.R) -> List(io1.REG2),
+    BitPat(SRCType.imm) -> List(io.Imm)
   ))
 
-  
+  switch(io.aluoptype){
+    is(ALUOPType.add){
+      io1.result := src1 + src2
+    }
+  }
 
 
 }
