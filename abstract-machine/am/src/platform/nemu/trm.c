3afd5fa11d69c45ba15ba10b42ae175fedd2c40d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-04 20:06:54 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:54 up  1:30,  1 user,  load average: 0.35, 0.32, 0.28

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -6,7 +6,7 @@ int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
-#define MAINARGS b//""
+#define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
@@ -23,5 +23,6 @@ void halt(int code) {
 
 void _trm_init() {
   int ret = main(mainargs);
+  printf("is here\n");
   halt(ret);
 }
