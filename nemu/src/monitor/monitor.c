37136b0767b39516ed6662e3d9de8821e9a79ad5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-04 20:19:39 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:19:39 up  1:43,  1 user,  load average: 0.64, 0.56, 0.41

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -81,7 +81,7 @@ static int parse_args(int argc, char *argv[]) {
   printf("is parse_args\n");
   
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
-    printf("%d\n",o);
+    printf("%d  %d\n",o,'b');
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
