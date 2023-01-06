0db11b1a67bdb4b6fcc2c847963559b6c7113799
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-06 21:17:49 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:17:49 up  3:36,  1 user,  load average: 1.10, 0.81, 0.44

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -50,7 +50,7 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  while(*s1==*s2){
+  while(*s1++ ==*s2++){
       if(*s1=='\0' && *s2=='\0'){return 0;}
   }
   return (*(unsigned char*)s1 - *(unsigned char*)s2);
