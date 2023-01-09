f29ae3580c0707bb8298d81f6eb87ef38e67353d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-09 15:46:32 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:32 up  3:34,  1 user,  load average: 0.37, 0.41, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -59,7 +59,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  printf("%x\n", s->isa.inst.val);
+  printf("%08x\n", s->isa.inst.val);
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
