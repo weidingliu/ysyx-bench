52c8efdce7ccff4c290a94e16b1e9ee9d8021cb6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-09 16:21:37 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:37 up  4:09,  1 user,  load average: 0.57, 0.53, 0.47

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -72,7 +72,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
-
+  for (i = ilen - 1; i >= 0; i --) {
+    p += snprintf(p, 4, " %02x", ibuf[irbuf_point].inst[i]);
+  }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
