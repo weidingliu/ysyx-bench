53e14db6d6d1b316c4a3bfc9e29c276ca0c725ff
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-09 12:44:09 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:44:09 up 32 min,  1 user,  load average: 0.85, 0.64, 0.64

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,35 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  //size_t num=0;//how many veriable argument
+  size_t ret=0;
+  for (int i=0;i<strlen(fmt);i++)
+  {
+      if(*fmt == '%'){
+          switch(*(fmt+1)){
+          case 'd':{
+              
+              fmt+=2;
+              break;
+          }
+          case 's':{
+              
+              fmt+=2;
+              break;
+          }
+          }
+      }
+      else{
+          *out++=*fmt++;
+          ret++;
+      }
+      
+  
+  }
+  
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
