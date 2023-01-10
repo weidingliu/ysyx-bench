94486ed5dbfcf01907b120d1828c8362f6066e89
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-10 14:18:11 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:18:11 up  1:51,  1 user,  load average: 1.04, 0.61, 0.49

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -160,7 +160,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-          if(nemu_state.state == NEMU_ABORT || nemu_state.halt_ret == 0) display_iringbuf();
+          if(nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0) display_iringbuf();
       // fall through
       
     case NEMU_QUIT: statistic();
