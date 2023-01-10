c934b230160ad0a441c47d708128dd8e9022a86b
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-10 13:48:04 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:48:04 up  1:21,  1 user,  load average: 0.30, 0.49, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,14 +43,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   printf("is here\n");
-  char te[128];
-  memset(ibuf[irbuf_point].ch_inst,'\0',sizeof(ibuf[irbuf_point].ch_inst));
-  strcpy(te, _this->logbuf);
-  strcpy(ibuf[irbuf_point].ch_inst,te);
+  //char te[128];
+  //memset(ibuf[irbuf_point].ch_inst,'\0',sizeof(ibuf[irbuf_point].ch_inst));
+  //strcpy(te, _this->logbuf);
+  //strcpy(ibuf[irbuf_point].ch_inst,te);
   //ibuf[irbuf_point].inst=(uint8_t *)&s->isa.inst.val;
   //ibuf[irbuf_point].pc=pc;
   irbuf_point=(irbuf_point+1)%IRTRACE;
-  printf("%d\n",ibuf[irbuf_point].ch_inst[0]);
+  printf("%d\n",_this->logbuf[0]);
   
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
  #ifdef CONFIG_WATCHPOINT
