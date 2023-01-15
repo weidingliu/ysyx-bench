c40583e80c11e6f674accf58a1c94116649e67a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 20:27:54 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:27:54 up  7:01,  3 users,  load average: 0.49, 0.45, 0.56

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -135,7 +135,7 @@ void init_ftrace(){
             funcINFO[ftrace_point].start=symtab[i].st_value;
             funcINFO[ftrace_point].size=symtab[i].st_size;
             strcpy(funcINFO[ftrace_point].fun_name,strtab+symtab[i].st_name);
-            printf("%s\n",funcINFO[ftrace_point].fun_name);
+            printf("%x\n",funcINFO[ftrace_point].start);
             //,funcINFO[ftrace_point].start,funcINFO[ftrace_point].size    %x  %ld
             
         }
