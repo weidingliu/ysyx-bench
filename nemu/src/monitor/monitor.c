d39b6bba00827e5b535d810055177b2f9efb13ee
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 16:49:00 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:49:00 up  3:22,  3 users,  load average: 0.42, 0.58, 0.61

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -78,11 +78,12 @@ void init_ftrace(){
     Elf64_Shdr elf_section_head;//loader section header
     fseek(fp, elf_head.e_shoff, SEEK_SET);
     o=fread(&elf_section_head,elf_head.e_shentsize,1,fp);
+    printf("--------------%d\n",elf_section_head.sh_name);
     Assert(o,"ELF section head fail!");
     Assert(elf_section_head.sh_type,"ELF section fail!");
     
     //Elf64_Sym sym_table;
-    printf("--------------%d\n",elf_section_head.sh_type);
+    
     
     
     strcpy(funcINFO[ftrace_point].fun_name,"hello");
