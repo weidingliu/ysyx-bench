a99c682fd82bc022814593589585e794004c7b72
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 21:45:12 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:45:12 up  4:11,  3 users,  load average: 0.57, 0.48, 0.39

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -72,7 +72,7 @@ static void func_trace(paddr_t pc,Decode *s){//head insert
             
             
             //printf("%d\n",t);
-            if((t & 0b1101111) !=0b1101111 ) {temp->type=0;return;}
+            if((t & 0b1101111) ==0b1101111 ) {temp->type=0;return;}
             if((t & 0b111000000000000)==0) {temp->type=1;return;}
             Assert(0,"bad ftrace!");    
         }
