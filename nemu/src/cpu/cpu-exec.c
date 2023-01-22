83303fa03642668ef1090910e46c71d20edc70c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-22 20:19:02 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:19:02 up  1:43,  1 user,  load average: 0.77, 0.49, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -34,10 +34,10 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 void device_update();
-
+#ifdef CONFIG_TRACE
 static f_link *ftr;
 static f_link *tail;
-
+#endif
 #ifdef CONFIG_ITRACE
 char ibuf[IRTRACE][128];
 static uint32_t irbuf_point=0;
@@ -192,8 +192,8 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-
-    func_trace(cpu.pc,&s);
+    
+    IFDEF(CONFIG_TRACE,func_trace(cpu.pc,&s));
     //if(funcINFO[2].start == cpu.pc) printf("%s\n",funcINFO[2].fun_name);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
@@ -241,7 +241,9 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+          #ifdef CONFIG_ITRACE
           if(nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0) display_iringbuf();
+          #endif
           #ifdef CONFIG_TRACE
           display_ftrace();
           #endif
