d25bb06369170008e4042c8e1fc93f3c7b4be115
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-23 19:44:04 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:44:04 up  6:16,  1 user,  load average: 0.26, 0.36, 0.29

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -19,7 +19,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   printf("%lx  %lx \n",pc,ref_r->pc);
-  if(pc != ref_r->pc ) return false;
+  if(pc != ref_r->pc+4 ) return false;
   for(int i=0;i<32;i++){
       if(ref_r->gpr[i]!=cpu.gpr[i]) return false;
   }
