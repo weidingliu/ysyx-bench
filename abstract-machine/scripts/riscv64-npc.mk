a9e5285bd6016ea76e7da6790c46d6edcad30797
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 19:53:41 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:53:41 up  1:37,  1 user,  load average: 0.67, 0.54, 0.43

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -1,5 +1,6 @@
 include $(AM_HOME)/scripts/isa/riscv64.mk
 NPC_PATH += $(subst nemu,npc/,$(NEMU_HOME))
+#ARGS ?= riscv64-npc
 
 AM_SRCS := riscv/npc/start.S \
            riscv/npc/trm.c \
@@ -10,7 +11,7 @@ AM_SRCS := riscv/npc/start.S \
            riscv/npc/trap.S \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
-ARGS ?= riscv64-npc
+
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
