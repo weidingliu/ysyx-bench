f5685a6098f060c3f836fba7811df05d9840c83c
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 19:52:49 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:52:49 up  1:37,  1 user,  load average: 0.47, 0.48, 0.40

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -1,4 +1,5 @@
 include $(AM_HOME)/scripts/isa/riscv64.mk
+NPC_PATH += $(subst nemu,npc/,$(NEMU_HOME))
 
 AM_SRCS := riscv/npc/start.S \
            riscv/npc/trm.c \
@@ -9,6 +10,7 @@ AM_SRCS := riscv/npc/start.S \
            riscv/npc/trap.S \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
+ARGS ?= riscv64-npc
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
@@ -20,3 +22,5 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+run: image
+	$(MAKE) -C $(NPC_PATH) sim 
--- npc/src/main/scala/One_cycle/Instruction.scala
+++ npc/src/main/scala/One_cycle/Instruction.scala
@@ -4,6 +4,6 @@ import chisel3.util._
 
 object Instruction extends paramete with InstrType {
     def NOP = 0x00000013.U
-    val Decoderfault =List(InstrN,FUType.alu,ALUOPType.ebreak)
+    val Decoderfault =List(InstrN,FUType.alu,ALUOPType.NOP)
   def Decodertable = RVI_ALU.table
 }
