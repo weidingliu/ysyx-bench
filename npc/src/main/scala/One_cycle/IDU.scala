545460e427a1ef3621ccb95c5d5270cd33778f25
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 20:18:48 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:18:48 up 30 min,  1 user,  load average: 2.27, 1.86, 1.21

--- npc/src/main/scala/One_cycle/IDU.scala
+++ npc/src/main/scala/One_cycle/IDU.scala
@@ -52,7 +52,7 @@ class IDU extends Module with InstrType with paramete{
 
   val srctype_list = Array(
     BitPat(InstrI) -> List(SRCType.R,SRCType.imm),
-    BitPat(InstrR) -> List(SRCType.PC,SRCType.R),
+//    BitPat(InstrR) -> List(SRCType.R,SRCType.R),
     BitPat(InstrU) -> List(SRCType.PC,SRCType.imm),
 
     BitPat(InstrN) -> List(SRCType.PC,SRCType.R),
