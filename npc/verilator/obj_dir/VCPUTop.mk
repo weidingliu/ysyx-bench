6114dce2849aae46bd0f2df2500bc877f47ff99d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 12:20:38 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:20:38 up  2:17,  1 user,  load average: 1.15, 1.03, 0.95

--- npc/verilator/obj_dir/VCPUTop.mk
+++ npc/verilator/obj_dir/VCPUTop.mk
@@ -45,7 +45,7 @@ VM_USER_CLASSES = \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
-	. \
+	src \
 
 
 ### Default rules...
@@ -57,7 +57,7 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-tb_CPUTop.o: tb_CPUTop.cpp
+tb_CPUTop.o: src/tb_CPUTop.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
