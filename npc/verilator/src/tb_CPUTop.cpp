c6e787eb6a8ed1657af922822b0bbd3e8e5d0685
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 12:45:05 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:45:05 up  2:41,  1 user,  load average: 1.08, 0.98, 0.96

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -120,7 +120,7 @@ while(sim_time<MAX_SIM_TIME && (!contextp->gotFinish())){
 printf("Final PC is : 0x%lx\n",dut->io_pc);
 dump_gpr();
 if(cpu_gpr[10] !=0) printf("\033[40;34mftrace: \033[0m \033[40;32mON\033[0m\n");
-else printf("\033[40;34mHIT GOOD: \033[0m \033[40;32m0x%lx\033[0m\n",dut->io_pc);
+else printf("\033[40;32mHIT GOOD: \033[0m \033[40;32m0x%lx\033[0m\n",dut->io_pc);
 m_trace->close();
 delete dut;
 delete contextp;
