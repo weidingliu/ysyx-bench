a270a31ef138a903926d8315139184b01b6d6002
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 22:22:16 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:22:16 up  6:09,  1 user,  load average: 0.47, 0.37, 0.46

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -99,6 +99,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   const char *p = s.c_str() + skip;
   assert((int)s.length() - skip < size);
   strcpy(str, p);
-  printf("%s\n",p);
+  //printf("%s\n",p);
+  //printf("%08x\n",inst);
   
 }
