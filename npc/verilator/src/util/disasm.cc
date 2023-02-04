c96fbed242d18c172ba6ca7edcb756355a629d38
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 22:33:21 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:33:21 up  6:20,  1 user,  load average: 0.89, 0.69, 0.57

--- npc/verilator/src/util/disasm.cc
+++ npc/verilator/src/util/disasm.cc
@@ -92,7 +92,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  printf("%08lx   %x\n",pc,&code);
+  //printf("%08lx   %x\n",pc,&code);
   printf("here2\n");
   
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
