9a4a58ce6cce7933c4d9fc3498dd79819235069c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 21:51:12 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:51:12 up  5:38,  1 user,  load average: 0.32, 0.55, 0.59

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -112,7 +112,7 @@ void exe_once(VCPUTop *s,VerilatedContext* contextp,VerilatedVcdC *m_trace){
     char *p;
     printf("12\n");
     //void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-    //disassemble(p,96,s->io_pc,(uint8_t *)&s->io_inst,4);
+    disassemble(p,96,s->io_pc,(uint8_t *)&s->io_inst,4);
     /*disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);*/
       
