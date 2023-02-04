ed410a7b7e4353ac316f2661782129aa3e82a960
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 21:06:14 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:14 up  4:53,  1 user,  load average: 2.18, 1.09, 0.74

--- npc/verilator/src/util/disasm.cc
+++ npc/verilator/src/util/disasm.cc
@@ -43,7 +43,7 @@ static llvm::MCDisassembler *gDisassembler = nullptr;
 static llvm::MCSubtargetInfo *gSTI = nullptr;
 static llvm::MCInstPrinter *gIP = nullptr;
 
-void init_disasm(const char *triple) {
+extern "C" void init_disasm(const char *triple) {
   llvm::InitializeAllTargetInfos();
   llvm::InitializeAllTargetMCs();
   llvm::InitializeAllAsmParsers();
@@ -87,7 +87,7 @@ void init_disasm(const char *triple) {
   gIP->setPrintBranchImmAsAddress(true);
 }
 
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
