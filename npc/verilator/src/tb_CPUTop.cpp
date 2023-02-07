e1138eb8a10edb7f30a6eaa58cf1ee7463119650
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 16:27:22 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:27:22 up  2:20,  1 user,  load average: 0.96, 0.92, 0.81

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -120,7 +120,7 @@ void exe_once(VCPUTop *s,VerilatedContext* contextp,VerilatedVcdC *m_trace){
     /*disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);*/
       
-    if(s->reset==0 && step_print_inst)printf("Addr: %08lx\t Inst: %s\t%08x\t\n",s->io_pc-4,p,Inst[0]);
+    if(s->reset==0 && step_print_inst)printf("Addr: %08lx\t Inst: %16s\t%08x\t\n",s->io_pc-4,p,Inst[0]);
 }
 
 void execute(VCPUTop *dut,VerilatedContext* contextp,VerilatedVcdC *m_trace,uint64_t n){
