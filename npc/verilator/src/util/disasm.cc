c2c46568ec427a849678bcd84f8acd386f29fbcd
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 15:34:14 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:34:14 up  1:27,  1 user,  load average: 0.35, 0.44, 0.35

--- npc/verilator/src/util/disasm.cc
+++ npc/verilator/src/util/disasm.cc
@@ -95,7 +95,15 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   //printf("%hhn\n",code);
   printf("%08lx   %hhn\n",pc,code);
   printf("here2\n");
-  printf("%d\n",gDisassembler->DecodeStatus);
+  printf("%d\n",);
+  
+  switch(gDisassembler->DecodeStatus){
+      case 0: printf("0\n");
+      case 1: printf("1\n");
+      case 3: printf("3\n");
+      default: printf("6\n");
+  }
+  
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 printf("here4\n");
   std::string s;
