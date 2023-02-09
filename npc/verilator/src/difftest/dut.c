2ac6d8037dcc7ce2c95dc7ba0d8e47b1485cd70f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-09 22:07:18 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:07:18 up  3:40,  1 user,  load average: 0.60, 0.83, 0.72

--- npc/verilator/src/difftest/dut.c
+++ npc/verilator/src/difftest/dut.c
@@ -5,7 +5,7 @@
 
 #include <tb.h>
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_memcpy)(uint64_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
