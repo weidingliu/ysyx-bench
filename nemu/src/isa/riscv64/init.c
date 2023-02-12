14d83788abe90fcbdf4cdca8de615a0c786616c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-12 15:53:23 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:53:23 up  3:38,  1 user,  load average: 0.62, 0.50, 0.53

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -37,7 +37,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   //printf("init isa1\n");
-  printf("%x\n",RESET_VECTOR);
+  printf("%x  %hhn\n",RESET_VECTOR,guest_to_host(RESET_VECTOR));
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
   printf("init isa\n");
   /* Initialize this virtual computer system. */
