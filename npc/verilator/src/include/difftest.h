52ca149af4c45aa5465da11a7a2bef797c3f64fe
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-12 16:38:58 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:38:58 up  4:23,  3 users,  load average: 0.61, 0.50, 0.48

--- npc/verilator/src/include/difftest.h
+++ npc/verilator/src/include/difftest.h
@@ -9,7 +9,7 @@ typedef void (*Ref_difftest_exec)(uint64_t n);
 typedef void (*Ref_difftest_raise_intr)(uint64_t NO);
 typedef void (*Ref_difftest_init)(int port);
 
-extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
+extern void (*ref_difftest_memcpy)(uint64_t addr, void *buf, size_t n, bool direction);
 extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(uint64_t NO);
