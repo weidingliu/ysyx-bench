3c833756cf0d8556be57ef87e94c69724d9702fd
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-12 20:33:51 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:33:51 up  8:18,  3 users,  load average: 3.27, 2.08, 1.25

--- npc/src/main/scala/One_cycle/RF.scala
+++ npc/src/main/scala/One_cycle/RF.scala
@@ -5,5 +5,5 @@ import chisel3.util._
 class RF extends paramete{
     val rf = Mem(NReg,UInt(xlen.W))
   def read (addr : UInt): UInt = Mux((addr === 0.U), 0.U,rf(addr))
-  def write (addr:UInt,data:UInt)  = {rf(addr) := data(xlen-1,0)}
+  def write (addr:UInt,data:UInt)  = {rf(addr) := Mux((addr ===0.U ),0.U,data(xlen-1,0))}
 }
