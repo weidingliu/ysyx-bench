b764c73b6ecdac1c458f9c53c0e2778686c9f285
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-14 20:46:15 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:46:15 up  1:28,  1 user,  load average: 0.73, 0.84, 0.81

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -133,7 +133,7 @@ void exe_once(VCPUTop *s,VerilatedContext* contextp,VerilatedVcdC *m_trace){
                 disassemble(p,96,s->io_pc,(uint8_t *)&s->io_inst,4);
       
                 if(s->reset==0 && step_print_inst)printf("Addr: %08lx\t Inst: %-16s\t%08x\t\n",s->io_pc,p,Inst[0]);
-                sprintf(ibuf[irbuf_point],"Addr: %08lx\t Inst: %-16s\t  %-8x\t\n",s->io_pc,p,s->io_inst);
+                sprintf(ibuf[irbuf_point],"Addr: %08lx\t Inst: %-16s\t  %8x\t\n",s->io_pc,p,s->io_inst);
     
     
                 irbuf_point=(irbuf_point+1)%IRTRACE;
