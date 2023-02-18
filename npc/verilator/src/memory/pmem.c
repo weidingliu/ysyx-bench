958b3144bd708c2fdf7f1cfcb692df44b9b8793d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-18 22:38:55 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:38:55 up  1:49,  1 user,  load average: 1.55, 0.77, 0.72

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -11,7 +11,7 @@ extern "C" void pmem_read(long long addr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   long long temp;
   memcpy(&temp,(pmem+(addr & ~0x7)),sizeof(temp));
-  rdata=&temp;
+  __attribute__ ((unused))rdata=&temp;
 }
 extern "C" void pmem_write(long long addr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
@@ -19,7 +19,7 @@ extern "C" void pmem_write(long long addr, long long wdata, char wmask) {
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
   long long *p=&wdata;
   uint8_t *temp=(uint8_t *)p;
-  int i=0
+  int i=0;
   while(wmask!=0){
       if(wmask & ~1){
           memcpy(pmem+(addr & ~0x7)+i,temp,sizeof(uint8_t));
