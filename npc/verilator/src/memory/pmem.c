bba38c9eb2a22133a7434903dc6ade31f4b13d35
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-18 22:54:06 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:54:06 up  2:04,  1 user,  load average: 0.83, 0.80, 0.85

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -11,15 +11,15 @@ extern "C" void pmem_read(long long addr, __attribute__((unused)) long long *rda
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   //printf("------%llx\n",addr);
   long long temp;
-  memcpy(&temp,(pmem+(addr & ~0x7)),sizeof(temp));
-  rdata=&temp;
+  //memcpy(&temp,(pmem+(addr & ~0x7)),sizeof(temp));
+  //rdata=&temp;
   
 }
 extern "C" void pmem_write(long long addr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  long long *p=&wdata;
+  /*long long *p=&wdata;
   uint8_t *temp=(uint8_t *)p;
   int i=0;
   while(wmask!=0){
@@ -29,7 +29,7 @@ extern "C" void pmem_write(long long addr, long long wdata, char wmask) {
       temp++;
       i++;
       wmask=wmask>>1;
-  }
+  }*/
   
   
 }
