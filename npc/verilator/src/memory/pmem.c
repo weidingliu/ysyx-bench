4fe9a756988d9d17414370df9af529d8e1d2a18f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 15:20:30 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:30 up 51 min,  3 users,  load average: 0.32, 0.47, 0.56

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -25,6 +25,7 @@ extern "C" void pmem_write(long long addr, long long wdata, char wmask) {
   int i=0;
   while(wmask!=0){
       if(wmask & 1){
+      printf("here\n");
           memcpy(pmem+(addr & ~0x7)-0x80000000+i,temp,sizeof(uint8_t));
       }
       temp++;
