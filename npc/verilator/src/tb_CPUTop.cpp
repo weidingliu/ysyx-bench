a276ac2b5ac1a393dca56ed7aed5c9e521d0cf08
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 22:08:09 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:09 up  2:17,  3 users,  load average: 0.67, 0.80, 0.72

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -30,7 +30,7 @@ static bool step_print_inst = false;
 vluint64_t sim_time=0;
 
 uint64_t *cpu_gpr=NULL;
-uint8_t mem[MAX_MEM];
+uint8_t mem[MAX_MEM]= {};
 
 uint32_t mem_size;
 uint32_t *Inst;
@@ -104,7 +104,7 @@ void init_mem(char *file_path){
         printf("%08x\n",mem[i]);
     }*/
     printf("load mem finish!\n");
-    //printf("%x\n",*(uint32_t *)(mem+234));
+    printf("%x\n",*(uint32_t *)(mem+0x24c));
 }
 
 //void ebreak() {dut->final();return;}
@@ -385,7 +385,7 @@ void sdb_main_loop(VCPUTop *s,VerilatedContext* contextp,VerilatedVcdC *m_trace)
 
 int main(int argc, char** argv) {
 //printf("--------------------%s   %d\n",argv[1],argc);
-memset(mem,0,sizeof(uint8_t) * MAX_MEM);
+//memset(mem,0,sizeof(uint8_t) * MAX_MEM);
 
 VerilatedContext* contextp = new VerilatedContext;
 contextp->commandArgs(argc, argv);
