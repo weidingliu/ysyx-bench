9e5ed9217a50c760ab4c2b1eace6ec1cb325396f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 20:26:20 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:20 up  1:50,  3 users,  load average: 0.50, 0.66, 0.75

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -11,9 +11,9 @@ uint8_t pmem[MAX_MEM];
 */
 extern "C" void pmem_read(long long addr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
-  printf("%016llx\n",(addr & ~0x7ull)-RESET_VECTOR);
+  printf("%016llx  %016llx\n",(addr & ~0x7ull)-RESET_VECTOR,addr);
   long long temp;
-  if((((addr & ~0x7ull)-RESET_VECTOR)>MAX_MEM) ) return;//assert(0);
+  if((((addr & ~0x7ull)-RESET_VECTOR)>MAX_MEM) ) assert(0);
   memcpy(&temp,(mem+(addr& ~0x7ull)-RESET_VECTOR),sizeof(long long));
   *rdata=temp;
   if(mtrace) printf("READ--- ADDR:  %016llx  DATA:  %016llx \n",(addr),*rdata);
