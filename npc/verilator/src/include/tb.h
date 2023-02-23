f97ee9aaff18ceb3677cd828e52b32790bb6e916
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 21:26:35 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:35 up  2:50,  3 users,  load average: 0.61, 0.95, 1.04

--- npc/verilator/src/include/tb.h
+++ npc/verilator/src/include/tb.h
@@ -17,6 +17,12 @@ typedef struct reg_cpu{
     uint64_t pc;
 }cpu_state;
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
 
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
