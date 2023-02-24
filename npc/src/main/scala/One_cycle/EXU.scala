9798a63f994fddf0414e5cc90282923f060b0176
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 22:34:19 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:34:19 up  3:38,  3 users,  load average: 0.66, 1.11, 1.11

--- npc/src/main/scala/One_cycle/EXU.scala
+++ npc/src/main/scala/One_cycle/EXU.scala
@@ -326,6 +326,9 @@ class EXU extends Module with paramete {
     is(ALUOPType.bne){
       dnpc := Mux(branch_flag===1.U,branch_result,io1.PC+4.U(xlen.W))
     }
+    is(ALUOPType.bge) {
+      dnpc := Mux(branch_flag === 1.U, branch_result, io1.PC + 4.U(xlen.W))
+    }
 
   }
 
