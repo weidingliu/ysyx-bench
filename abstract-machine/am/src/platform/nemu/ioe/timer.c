90c22f3e89efff26c4db3d77cbce128f83c66fcb
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-26 20:58:44 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:44 up  1:33,  1 user,  load average: 0.96, 0.62, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -17,7 +17,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //uptime->us = seconds * 1000000 + (useconds + 500);
   
   uint64_t t=inl(RTC_ADDR);
-  uint64_t t2=inl(RTC_ADDR+4);
+  uint64_t t2=inl(RTC_ADDR+1);
   uint64_t p=(t2 << 32)+t;
   uptime->us = p;
   //t[1]=inl(RTC_ADDR+4);
