2ee773cbef95c14fb13a5961a0898a7922a35152
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-27 22:15:24 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:24 up  2:43,  2 users,  load average: 0.50, 0.67, 0.70

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -34,7 +34,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
       
       for(int i=0;i<32;i++){
           if(!strcmp(s,regs[i])){
-              return *regs[i];
+              return cpu.gpr[i];
           }
       }
   *success=false;
