4749a4d907064f970c7e1fa9d9d8c340938f5f08
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 22:21:23 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:21:23 up  4:35,  3 users,  load average: 0.46, 0.54, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,13 +32,13 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     if(ctl->w==0 || ctl->h==0) return;
     int i=ctl->y,j=ctl->x;
     outl(SYNC_ADDR,1);
-   
+   printf("%d  %d   %d   %d \n",i,j,ctl->w,ctl->h);
    for(;i<ctl->h;i++){
         for(;j<ctl->w;j++){
             outl(FB_ADDR+(i*ctl->h+j)*4, *(uint32_t *)(ctl->pixels + (i*ctl->h+j)*4));
         }
     }
-    printf("%d  %d   %d   %d \n",i,j,ctl->w,ctl->h);
+    
   
 }
 
