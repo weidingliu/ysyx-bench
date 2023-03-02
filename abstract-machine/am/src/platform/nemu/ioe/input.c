a25ff60861af1ce25759b0c56f05d04cdf3811d2
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 19:25:00 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:25:00 up  1:38,  3 users,  load average: 0.64, 0.88, 0.63

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,7 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   
   uint32_t k=inl(KBD_ADDR);
-  printf("%d\n",k);
+  printf("%08x\n",k);
   if(k!=AM_KEY_NONE){
       kbd->keycode = k;
       kbd->keydown = 1;
