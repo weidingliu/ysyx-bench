5dd2c62a3e85e3f85da447477ea1c9b6127da94d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 19:24:35 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:35 up  1:38,  3 users,  load average: 0.71, 0.91, 0.63

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,11 +1,13 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   
   uint32_t k=inl(KBD_ADDR);
+  printf("%d\n",k);
   if(k!=AM_KEY_NONE){
       kbd->keycode = k;
       kbd->keydown = 1;
