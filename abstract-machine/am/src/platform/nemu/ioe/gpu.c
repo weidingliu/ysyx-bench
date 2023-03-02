6b563d1684174066886ef104ad7861611c39ae0d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 21:54:39 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:39 up  4:08,  3 users,  load average: 0.68, 0.52, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,7 +9,7 @@ void __am_gpu_init() {
   uint32_t v1=inl(VGACTL_ADDR);
   printf("%08x\n",v1);
   int w = (v1 >> 16)*2;  // TODO: get the correct width
-  int h = (v1 % 256)*2;  // TODO: get the correct height
+  int h = (v1 % (2^16))*2;  // TODO: get the correct height
   printf("%d  %d\n",w,h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
