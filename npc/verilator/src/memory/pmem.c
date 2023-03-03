dc1082c66d2be58a17242a80200f73ea51a51bcc
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 14:48:01 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:01 up  5:12,  3 users,  load average: 0.70, 0.73, 0.76

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -10,7 +10,7 @@ bool is_skip_ref=0;
 
 static uint64_t get_time_internal(){
     struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC_COARSE,&now);
+    clock_gettime(CLOCK_MONOTONIC,&now);
     uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
     return us;
     
