c83577340c5c64fdda7ae45a5c8a2706cb0ee4ed
liuweidin <1045251744@qq.com>
2023-03-05 22:25:55 +0800

    On branch pa3  Changes to be committed: 	modified:   abstract-machine/am/include/arch/riscv64-nemu.h 	modified:   abstract-machine/klib/src/stdio.c 	modified:   nanos-lite/include/common.h 	modified:   nemu/src/isa/riscv64/inst.c 	modified:   nemu/src/isa/riscv64/system/intr.c

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -19,6 +19,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+   Context.mepc=epc;
+   Context.mcause=Event,event.EVENT_YIELD;
 
   return 0;
 }
