0ea3a69704cca45f21b9aff54ee94934e62ec919
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 12:27:59 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:27:59 up  2:15,  1 user,  load average: 0.41, 0.47, 0.46

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -53,7 +53,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_R: src1R(); src2R();         break;
   }
-  printf("----%08lx\n",*src1);
+  printf("----%08lx,%d,%d\n",*src1,type,TYPE_I);
   printf("%d\n",csrid);
 }
 
