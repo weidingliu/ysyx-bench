405ecdae5c43af00b5e59d875d1d2c7cec673a94
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-07 20:14:19 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:19 up  1:37,  1 user,  load average: 0.84, 0.54, 0.40

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,13 +8,14 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case(11): ev.event = EVENT_YIELD;break;
       default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
+  printf("%lx %lx %lx \n",c->mcause,c->mstatus,c->mepc);
   return c;
 }
 
