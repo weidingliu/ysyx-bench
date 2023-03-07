e45be5e8655cdf89c42d47c785395f0eebecddc1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-07 20:55:32 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:32 up  2:18,  1 user,  load average: 0.58, 0.38, 0.35

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,8 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case(EVENT_YIELD): printf("I love PA\n");
-    default: panic("Unhandled event ID = %d", e.event);
+    case(EVENT_YIELD): printf("I love PA\n");break;
+    default: panic("Unhandled event ID = %d", e.event);break;
   }
 
   return c;
