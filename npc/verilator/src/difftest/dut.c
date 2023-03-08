e9a8d8db0fc76672fcc0703923035a29989f3d51
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 12:03:29 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:03:29 up  2:35,  1 user,  load average: 0.74, 0.78, 0.79

--- npc/verilator/src/difftest/dut.c
+++ npc/verilator/src/difftest/dut.c
@@ -20,7 +20,7 @@ void init_difftest(char *ref_so_file, uint32_t img_size, int port, uint8_t *mem)
   void *handle;
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
-  
+  printf("%d\n",img_size);
   ref_difftest_memcpy = (Ref_difftest_memcpy)dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
   
@@ -37,7 +37,7 @@ void init_difftest(char *ref_so_file, uint32_t img_size, int port, uint8_t *mem)
   assert(ref_difftest_init);
 
   ref_difftest_init(port);
-  printf("%d\n",img_size);
+  
   ref_difftest_memcpy(RESET_VECTOR, mem, img_size, DIFFTEST_TO_REF);
   //printf("here5\n");
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
