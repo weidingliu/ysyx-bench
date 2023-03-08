dcc6fc08678840219930eda9c19dcb1e2b314282
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 10:56:56 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:56:56 up  1:29,  1 user,  load average: 0.70, 0.73, 0.69

--- npc/verilator/src/difftest/dut.c
+++ npc/verilator/src/difftest/dut.c
@@ -67,7 +67,7 @@ bool difftest_step(uint64_t pc){
         is_skip_ref=0;
         return true;
     }
-    printf("dsaf\n");
+    printf("dsaf %d\n",is_skip_ref);
     cpu_state ref_cpu;
     ref_difftest_exec(1);
     ref_difftest_regcpy(&ref_cpu, DIFFTEST_TO_DUT);
