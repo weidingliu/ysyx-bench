f4ad939a6b217dd4bae428430bb51aa770bff83f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 15:47:59 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:47:59 up  5:13,  1 user,  load average: 1.99, 1.03, 0.83

--- npc/src/main/scala/One_cycle/CSR.scala
+++ npc/src/main/scala/One_cycle/CSR.scala
@@ -2,10 +2,10 @@ package One_cycle
 import chisel3._
 
 object CSR_index {
-  def mstatus = "0x300".U
-  def mtvec ="0x305".U
-  def mepc ="0x341".U
-  def mcause ="0x342".U
+  def mstatus = 0x300.U
+  def mtvec =0x305.U
+  def mepc =0x341.U
+  def mcause =0x342.U
 
   def apply() =UInt(12.W)
 }
