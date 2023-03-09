65f11443f075cf7986c03ffe5142504d1bc373f0
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 14:03:09 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:09 up  3:28,  1 user,  load average: 0.27, 0.49, 0.62

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -28,15 +28,21 @@ static inline int check_reg_idx(int idx) {
 }*/
 
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
-#define csrgpr(idx)                          \
-    do{                                      \
-      if(idx==0x00) return cpu.mstatus;      \
-      else if(idx==0x05) return cpu.mtvec;   \
-      else if(idx == 0x41) return cpu.mepc;  \
-      else if(idx==0x42) return cpu.mcause;   \
-      else assert(0);                         \
-    }while(0)               
-
+word_t csrgpr_read(int idx) {
+      if(idx==0x00) return cpu.mstatus;               
+      else if(idx==0x05) return cpu.mtvec;     
+      else if(idx == 0x41) return cpu.mepc;   
+      else if(idx==0x42) return cpu.mcause;
+      else assert(0);
+     
+}
+void csrgpr_write(int idx,word_t csr_data){
+    if(idx==0x00) cpu.mstatus=csr_data;               
+      else if(idx==0x05) cpu.mtvec=csr_data;     
+      else if(idx == 0x41) cpu.mepc=csr_data;   
+      else if(idx==0x42) cpu.mcause=csr_data;
+      else assert(0);
+}
 
 static inline const char* reg_name(int idx, int width) {
   extern const char* regs[];
