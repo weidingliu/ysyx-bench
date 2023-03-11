06655448a3a11420ea18a4ed4232d63d7bc0e988
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 17:22:36 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:36 up  7:05,  1 user,  load average: 0.77, 0.74, 0.77

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -188,7 +188,7 @@ void execute(VCPUTop *dut,VerilatedContext* contextp,VerilatedVcdC *m_trace,uint
     while(n--!=0 &&((!contextp->gotFinish()))){
         exe_once(dut,contextp,m_trace);
         
-        if(DIFFTEST && !is_skip_ref && !ref_is_irq){
+        if(DIFFTEST && !ref_is_irq){
             bool flag=difftest_step(dut->io_pc);
         
             if(!flag) {state=ABORT; break;}
