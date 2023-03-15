0527a890f64af5daa2acfdd2d15d34057c56214e
liuweidin <1045251744@qq.com>
2023-03-15 22:10:11 +0800

    On branch pa3  Changes to be committed: 	modified:   nanos-lite/include/proc.h 	modified:   nanos-lite/src/loader.c 	modified:   nanos-lite/src/proc.c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,11 @@
 #include <proc.h>
 #include <elf.h>
 
+
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -10,7 +15,14 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  printf("-----------------------here\n");
+  Elf_Ehdr elf_head;
+  size_t o __attribute__((unused));
+  memcpy(&elf_head,&ramdisk_start,sizeof(Elf_Ehdr));
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
+  memcpy(phdr,&ramdisk_start+elf_head.e_phoff);
   TODO();
+ 
   return 0;
 }
 
