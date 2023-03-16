380881931cc38845182db366f1d9f6f634e71f1b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 16:10:39 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:39 up  2:11,  3 users,  load average: 0.40, 0.43, 0.53

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,13 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case(11): ev.event = EVENT_YIELD;c->mepc+=4;break;
+      case(11): {
+          if(c->gpr[17]==-1) ev.event = EVENT_YIELD;
+          else ev.event =EVENT_SYSCALL;
+          //printf("%d\n",ev.event);
+          c->mepc+=4;
+          break;
+      }
       default: ev.event = EVENT_ERROR; break;
     }
     /*printf("%x %x %x %x\n",c->mcause,c->mstatus,c->mepc,c->pdir);
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <common.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,8 +11,31 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elf_head;
+  size_t o __attribute__((unused));
+  o=ramdisk_read(&elf_head,0,sizeof(Elf_Ehdr));
+  assert(o);
+  //printf("%x\n",*(uint32_t *)elf_head.e_ident);
+  assert(*(uint32_t *)elf_head.e_ident == 0x464c457f);
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
+  
+  o=ramdisk_read(phdr,elf_head.e_phoff,sizeof(Elf_Phdr) * elf_head.e_phnum);
+  assert(o);
+  for(int i=0;i<elf_head.e_phnum;i++){
+      if(phdr[i].p_type == 1){
+          //printf("%x\n",phdr[i].p_vaddr);
+          if(phdr[i].p_memsz==0) continue;
+          char buf[phdr[i].p_memsz];
+          //printf("%d  %d\n",sizeof(buf),phdr[i].p_memsz);
+          ramdisk_read(buf,phdr[i].p_offset,phdr[i].p_memsz);
+          memcpy((void *)phdr[i].p_vaddr,buf,phdr[i].p_memsz);
+          assert(phdr[i].p_memsz-phdr[i].p_filesz>=0);
+          //printf("%x  %x  %x\n",phdr[i].p_vaddr+phdr[i].p_filesz,phdr[i].p_vaddr,phdr[i].p_filesz);
+          memset((void *)(phdr[i].p_vaddr+phdr[i].p_filesz),0,phdr[i].p_memsz-phdr[i].p_filesz);
+      }
+  }
+ 
+  return elf_head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
