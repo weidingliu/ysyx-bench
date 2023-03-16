afab35309bcc5387756dde99da7306f95842e35c
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 20:19:07 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:07 up  6:19,  3 users,  load average: 0.72, 0.52, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(o);
   //printf("%x\n",*(uint32_t *)elf_head.e_ident);
   assert(*(uint32_t *)elf_head.e_ident == 0x464c457f);
-  //printf("%d\n",elf_head.e_machine);
+  printf("%d\n",elf_head.e_machine);
+  
   assert(elf_head.e_machine == EXPECT_TYPE);
   Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
   o=ramdisk_read(phdr,elf_head.e_phoff,sizeof(Elf_Phdr) * elf_head.e_phnum);
