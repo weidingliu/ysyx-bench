1affe4d71b2417aff16aa35c240d53407cbac98a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 20:44:43 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:43 up  6:45,  3 users,  load average: 0.61, 0.56, 0.49

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,13 +5,19 @@ int sys_yield(){
     return 0;
 }
 
+void sys_exit(){
+    halt(1);
+    return;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("%d\n",a[0]);
+  //printf("%d\n",a[0]);
   switch (a[0]) {
     case(SYS_yield): {sys_yield();break;}
     case(-1): break;
+    case(SYS_exit): sys_exit();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
