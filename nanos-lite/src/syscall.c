fd67780453e5e857f2972d12fd3dabaf27bb7b18
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 21:23:57 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:57 up  1:24,  3 users,  load average: 0.62, 0.42, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,15 +10,29 @@ int sys_exit(void *state){
     return 0;
 }
 
+size_t sys_write(int fd,const void *buf,size_t count){
+    size_t o=0;
+    if(fd!=1 && fd!=2) {panic("Unhandled FD =%d",fd); return -1;}//1 stdout 2 stderr
+    for(int i=0;i<count;i++){
+        putch(*(uint8_t *)buf);
+        buf++;
+        o++;
+    }
+    return o;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
   //printf("%d\n",a[0]);
-  int ret;
+  int ret=-1;
   switch (a[0]) {
     case(SYS_yield): {ret=sys_yield();break;}
     case(-1): break;//??
     case(SYS_exit): ret=sys_exit(NULL);break;
+    case(SYS_write): ret=sys_write(1,(void *)a[1],a[2]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx=ret;
