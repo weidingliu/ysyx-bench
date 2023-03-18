da7fd43d283d3fd9568284484aae054f94a48361
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 13:01:28 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:28 up  2:35,  3 users,  load average: 0.37, 0.47, 0.46

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,25 @@
 #include <common.h>
 #include "syscall.h"
+
+const char *enum_name[]={
+      "SYS_exit","SYS_yield","SYS_open","SYS_read","SYS_write","SYS_kill","SYS_getpid","SYS_close",
+  "SYS_lseek","SYS_brk","SYS_fstat","SYS_time","SYS_signal","SYS_execve","SYS_fork","SYS_link",
+  "SYS_unlink","SYS_wait","SYS_times","SYS_gettimeofday"
+};
+
+const char *get_name(int value){
+    if(value<0 || value>SYS_gettimeofday) panic("Fail nemu\n");
+    return enum_name[value];
+}
+
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case(EVENT_YIELD): do_syscall(c); break;
     case(EVENT_SYSCALL): do_syscall(c);  break;
     default: panic("Unhandled event ID = %d", e.event);break;
   }
-  printf("syscall %d  = %x\n",e.event,c->GPRx);  //strace
+  printf("syscall %s  = %x\n",get_name(c->GPR1),c->GPRx);  //strace
   return c;
 }
 
