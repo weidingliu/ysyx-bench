5d83e71507b7bd561ff31dac70f3470d30b6fdaa
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 16:32:29 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:32:29 up  6:06,  3 users,  load average: 0.33, 0.41, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -236,7 +236,7 @@ void cpu_exec(uint64_t n) {
 
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
-  if(n>20000) display_ftrace();
+  //if(n>20000) display_ftrace();
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     
