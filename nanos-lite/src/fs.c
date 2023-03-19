38cbd6cd1d28629bde10760ba53a383395d467ae
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 16:47:30 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:30 up  2:10,  1 user,  load average: 0.81, 0.68, 0.65

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -59,15 +59,17 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
     if(file_table[fd].disk_offset+file_table[fd].open_offset+len>get_ramdisk_size() || file_table[fd].disk_offset+file_table[fd].open_offset+len<0) panic("should not reach here");
     if(file_table[fd].open_offset+len>file_table[fd].disk_offset+file_table[fd].size) panic("should not reach here");
-    file_table[fd].open_offset+=len;
+    
     size_t ret=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+    file_table[fd].open_offset+=len;
     return ret;
 }
 size_t fs_write(int fd, const void *buf, size_t len){
     if(file_table[fd].disk_offset+file_table[fd].open_offset+len>get_ramdisk_size() || file_table[fd].disk_offset+file_table[fd].open_offset+len<0) panic("should not reach here");
     if(file_table[fd].open_offset+len>file_table[fd].disk_offset+file_table[fd].size) panic("should not reach here");
+    size_t ret =ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
     file_table[fd].open_offset+=len;
-    return ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+    return ret;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence){
