565d55aaf5093b0232f0293aa6b544c68fbbafe3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 17:36:15 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:15 up  2:59,  1 user,  load average: 0.44, 0.46, 0.56

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -58,8 +58,8 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  intptr_t ret =_syscall_(SYS_open, (intptr_t)path, flags, mode);
+  return ret;
 }
 
 int _write(int fd, void *buf, size_t count) {
@@ -86,8 +86,8 @@ int _close(int fd) {
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  return 0;
+  intptr_t ret =_syscall_(SYS_lseek, offset, whence, 0);
+  return ret;
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
