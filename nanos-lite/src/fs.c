d4428c3e25779436526e1cd8a464593fbb582d22
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 16:21:39 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:39 up  4:30,  1 user,  load average: 0.52, 0.54, 0.64

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,6 +64,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
+    printf("asdfasdf\n");
     int num=sizeof(file_table)/sizeof(file_table[0]);
     if(fd>num || fd<0) panic("should not reach here");
     if(file_table[fd].read != NULL){
