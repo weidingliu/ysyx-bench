76c9aa91c701f42af1cb8046cf629259a41a9f79
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 16:34:52 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:52 up  4:44,  1 user,  load average: 0.80, 0.64, 0.63

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -21,7 +21,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   FILE *fp = fopen("/dev/events", "r");
-  //printf("----%d\n",*(int *)fp);
+  printf("----%d\n",*(int *)fp);
   assert(fp);
   
   int o=fscanf(fp,"%s\n",buf);
