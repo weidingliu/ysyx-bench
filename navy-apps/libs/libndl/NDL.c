9783e29ea56b8896358c39c14f452b0d6c13105b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 16:08:10 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:10 up  4:17,  1 user,  load average: 0.96, 0.76, 0.80

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -19,15 +19,16 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
+  printf("asdfasf\n");
   FILE *fp = fopen("/dev/events", "r");
   //printf("----%d\n",*(int *)fp);
   assert(fp);
-  printf("asdfasf\n");
+  
   int o=fscanf(fp,"%s\n",buf);
   printf("%d\n",o);
   if(o==0) {fclose(fp); return 0;}
   fclose(fp);
-  return 0;
+  return 1;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
