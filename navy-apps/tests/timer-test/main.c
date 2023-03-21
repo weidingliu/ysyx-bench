a3b72a5f2cfdb5a2787d477986201b6ff5b16a51
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 13:31:58 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:31:58 up  1:41,  1 user,  load average: 0.64, 0.73, 0.55

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,14 +6,14 @@ int main(){
     
     struct timeval tv;
     struct timezone tz;
-    int times=0;
+    int times=1;
     //printf("sadf\n");
     while(1){
         do{
-            printf("sadf\n");
+            //printf("sadf\n");
             gettimeofday(&tv,&tz);
-            printf("sadf %ld\n",tv.tv_usec);
-        }while(tv.tv_usec % 500000 !=0);
+            //printf("sadf %ld\n",tv.tv_usec);
+        }while((tv.tv_usec) / 500000 < times);
         printf("loop in %d times\n",times++);
     }
     
