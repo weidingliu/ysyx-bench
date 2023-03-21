bcd634f65342965a3500a5b3bb1ca946e6d64b99
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 12:46:56 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:46:56 up 56 min,  1 user,  load average: 0.46, 0.62, 0.60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -40,6 +40,20 @@ int sys_close(int fd){
     return fs_close(fd);
 }
 
+int sys_gettimeofday(struct timeval *tv, struct timezone *tz){
+   AM_TIMER_RTC_T rtc;
+   rtc = io_read(AM_TIMER_RTC);
+   if(tv!=NULL){
+       tv->tv_sec = rtc.second;
+       tv->tv_usec = io_read(AM_TIMER_UPTIME).us;
+   }
+   if(tz!=NULL){
+       tz->tz_minuteswest = 0;
+       tz->tz_dsttime = 0;
+   }
+   return 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -58,6 +72,7 @@ void do_syscall(Context *c) {
     case(SYS_lseek): ret=sys_lseek(a[1],a[2],a[3]); break;
     case(SYS_read): ret=sys_read(a[1],(void *)a[2],a[3]);break;
     case(SYS_close): ret=sys_close(a[1]);break;
+    case(SYS_gettimeofday): ret=sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   //printf("%d\n",ret);
