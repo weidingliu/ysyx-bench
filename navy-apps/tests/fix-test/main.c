92a7871f017e168c88961fa5d9709c7fab624f9b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 17:24:00 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:24:00 up  4:45,  3 users,  load average: 0.42, 0.46, 0.44

--- navy-apps/tests/fix-test/main.c
+++ navy-apps/tests/fix-test/main.c
@@ -9,10 +9,10 @@ int main(){
     int c=0;
     int d =2;
     printf("%d\n",fixedpt_toint(fixedpt_floor(a)));
-    assert(fixedpt_toint(fixedpt_floor(a)) !=1);
-    assert(fixedpt_toint(fixedpt_floor(b)) !=10);
-    assert(fixedpt_toint(fixedpt_floor(fixedpt_fromint(c))) !=0);
-    assert(fixedpt_toint(fixedpt_floor(fixedpt_fromint(d))) !=2);
+    assert(fixedpt_toint(fixedpt_floor(a)) ==1);
+    assert(fixedpt_toint(fixedpt_floor(b)) ==10);
+    assert(fixedpt_toint(fixedpt_floor(fixedpt_fromint(c))) ==0);
+    assert(fixedpt_toint(fixedpt_floor(fixedpt_fromint(d))) ==2);
     
     printf("PASS!\n");
     return 0;
