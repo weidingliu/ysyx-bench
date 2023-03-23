164a5698a417e1bead025a39447862ee5de67f38
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 20:38:26 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:38:26 up  8:00,  3 users,  load average: 0.81, 0.73, 0.55

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,9 @@
 int main() {
   NDL_Init(0);
   int w, h;
+  printf("sdafggghh\n");
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("sdafggghh\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
