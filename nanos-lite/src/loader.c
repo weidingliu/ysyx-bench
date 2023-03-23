d1a6931cd0ba93ca3435d8e58de8a6587da5f368
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 23:33:13 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:13 up 10:54,  3 users,  load average: 0.63, 0.58, 0.62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,18 +45,18 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
   o=fs_read(fd,phdr,sizeof(Elf_Phdr) * elf_head.e_phnum);
   assert(o);
-  
+  char *buf=NULL;
   for(int i=0;i<elf_head.e_phnum;i++){
       if(phdr[i].p_type == 1){
           //printf("%x\n",phdr[i].p_vaddr);
           if(phdr[i].p_memsz==0) continue;
-          char buf[phdr[i].p_memsz];
+          
           
           o=fs_lseek(fd,phdr[i].p_offset,SEEK_SET); 
           assert(o!=-1);
           
           o=fs_read(fd,buf,phdr[i].p_memsz);
-          printf("fdsf\n");
+          
           assert(o);
           
           memcpy((void *)phdr[i].p_vaddr,buf,phdr[i].p_memsz);
