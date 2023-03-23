9ce92a7968a5b572706ee0c0c790ff08c49b6c03
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 23:25:47 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:47 up 10:47,  3 users,  load average: 0.35, 0.66, 0.63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,6 +34,7 @@ size_t sys_lseek(int fd, size_t offset, int whence){
 }
 size_t sys_read(int fd, void *buf, size_t len){
     if(fd ==1 || fd ==2) panic("should not here!");
+    printf("%x\n",&buf);
     return fs_read(fd,buf,len);
 }
 
