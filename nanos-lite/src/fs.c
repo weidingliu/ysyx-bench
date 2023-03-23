662505e61ac607cfa640f872619b7453c2044ffa
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 22:37:22 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:37:22 up  9:59,  3 users,  load average: 0.30, 0.54, 0.65

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,12 +82,13 @@ size_t fs_read(int fd, void *buf, size_t len){
     if(file_table[fd].open_offset+len>file_table[fd].disk_offset+file_table[fd].size) panic("should not reach here");
     
     size_t ret =0;
+    printf("%x\n",&buf);
     if(file_table[fd].open_offset+len > file_table[fd].size){
         ret=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,file_table[fd].open_offset+len - file_table[fd].size);
         file_table[fd].open_offset+=file_table[fd].open_offset+len - file_table[fd].size;
     }
     else{
-        printf("%x\n",&buf);
+        
         ret=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
         file_table[fd].open_offset+=len;
     }
