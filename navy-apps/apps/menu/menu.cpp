4832ca07b2d889c85b06c616c5b0ed65bca18478
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 18:55:39 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:39 up  6:03,  1 user,  load average: 0.43, 0.41, 0.32

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -64,7 +64,9 @@ static void prev() {
 }
 
 static void clear_display(void) {
+  printf("asdfasf\n");
   SDL_FillRect(screen, NULL, 0xffffff);
+  printf("asdfasf\n");
 }
 
 int main(int argc, char *argv[], char *envp[]) {
@@ -142,7 +144,7 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  printf("asdfasf\n");
+  
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
