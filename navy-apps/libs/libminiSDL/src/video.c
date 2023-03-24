84dfdce8e045bb817018025c5d7839ed26e3ca70
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 19:52:42 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:42 up  7:00,  1 user,  load average: 0.73, 0.45, 0.35

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -34,7 +34,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
       src_i++;
       if(src_i>=src_h) break;
   }
-  printf("fsadgfggg %d %d %d %d %d %d\n",dst_x, dst_y,  dst_w,  dst_h,dstrect->x,dstrect->y);
+  printf("fsadgfggg %d %d %d %d %d %d\n",dst_x, dst_y,  dst_w,  dst_h,dstrect->w,dstrect->h);
   NDL_DrawRect((uint32_t *)dst->pixels, dst_x, dst_y,  dst_w,  dst_h);
 }
 
