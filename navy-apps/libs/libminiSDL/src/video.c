69baecda988a2e0b845fcb601e016887613cfec3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 14:53:44 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:44 up  2:01,  1 user,  load average: 0.41, 0.46, 0.58

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -14,7 +14,7 @@ panic("should not reach here");
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-panic("should not reach here");
+    NDL_DrawRect((uint32_t *)s->pixels, x, y,  w,  h);
 }
 
 // APIs below are already implemented.
