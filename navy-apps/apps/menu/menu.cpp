d73c4ab6e9c73956eb9a00d140e6692fa6f1d0e8
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 18:45:52 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:52 up  5:53,  1 user,  load average: 0.57, 0.31, 0.24

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -75,10 +75,10 @@ int main(int argc, char *argv[], char *envp[]) {
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
   set_i_max();
-  printf("sdaf\n");
+
   while (1) {
     display_menu(i_max);
-    printf("sdaf\n");
+    
     SDL_Event e;
     do {
       SDL_WaitEvent(&e);
@@ -142,6 +142,7 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
+  printf("sdaf\n");
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
