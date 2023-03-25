74a53e3536fb260c86b3a2e3ab4fc7db86e7b940
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-25 22:13:53 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:53 up  4:50,  3 users,  load average: 0.57, 0.47, 0.51

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -7,30 +7,30 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
 }
 
 void SDL_CloseAudio() {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
 
 void SDL_PauseAudio(int pause_on) {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
   return NULL;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
 
 void SDL_LockAudio() {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
 
 void SDL_UnlockAudio() {
-  printf("should not reach here\n");
+  printf("should not reach here audio\n");
 }
