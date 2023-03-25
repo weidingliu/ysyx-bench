48fe971cbde89815cd7a5226c6d338cff2a08dd4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-25 22:28:03 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:03 up  5:04,  3 users,  load average: 0.22, 0.34, 0.37

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -11,7 +11,7 @@ static const char *keyname[] = {
 };
 
 int SDL_PushEvent(SDL_Event *ev) {
-  printf("should not reach here\n");
+  printf("should not reach here event\n");
   return 0;
 }
 
@@ -61,7 +61,7 @@ int SDL_WaitEvent(SDL_Event *event) {
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  printf("should not reach here\n");
+  printf("should not reach here event\n");
   return 0;
 }
 
--- navy-apps/libs/libminiSDL/src/file.c
+++ navy-apps/libs/libminiSDL/src/file.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 SDL_RWops* SDL_RWFromFile(const char *filename, const char *mode) {
-  printf("should not reach here\n");
+  printf("should not reach here file\n");
   return NULL;
 }
 
 SDL_RWops* SDL_RWFromMem(void *mem, int size) {
-  printf("should not reach here\n");
+  printf("should not reach here file\n");
   return NULL;
 }
