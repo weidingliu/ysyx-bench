d7530be67b15d9eb54b551d010182a58f58633c8
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-30 15:03:56 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:56 up  4:26,  3 users,  load average: 1.44, 0.84, 0.61

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -109,7 +109,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 void *memcpy(void *out, const void *in, size_t n) {
   int num=n;
   //printf("%x  %d %x\n",&out,n,(intptr_t)out);
-  printf("here\n");
+  //printf("here\n");
   //if(((intptr_t)out) == 0x800c2780) assert(0);
   while(num--){
           *(char *)out = *(char *)in;
