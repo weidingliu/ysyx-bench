ce556e91ff5ca1c64a72eaf93b615b568a75599d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-30 14:54:50 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:50 up  4:17,  3 users,  load average: 0.49, 0.45, 0.46

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -135,7 +135,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   //puts(_this->logbuf);
   //printf("%d\n",(int) cpu.gpr[15]);
   IFDEF(CONFIG_ITRACE,strcpy(ibuf[irbuf_point],_this->logbuf));
-  puts(ibuf[irbuf_point]);
+  //puts(ibuf[irbuf_point]);
   IFDEF(CONFIG_ITRACE,irbuf_point=(irbuf_point+1)%IRTRACE);
   //printf("-------------%08x\n",_this->isa.inst.val);
   //printf("----------------%016lx\n",dnpc);
@@ -265,5 +265,6 @@ void cpu_exec(uint64_t n) {
       // fall through
       
     case NEMU_QUIT: {statistic();}
+    display_iringbuf();
   }
 }
