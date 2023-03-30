f8c0dab648c34a2ff6be0fd53f2da95716222bdd
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-30 15:54:26 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:26 up  5:17,  3 users,  load average: 0.76, 0.76, 0.84

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -92,7 +92,7 @@ size_t fs_read(int fd, void *buf, size_t len){
         ret=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
         file_table[fd].open_offset+=len;
     }
-    printf("%x\n",*(uint32_t *)(buf+0x2b3c));
+    
     return ret;
 }
 size_t fs_write(int fd, const void *buf, size_t len){
