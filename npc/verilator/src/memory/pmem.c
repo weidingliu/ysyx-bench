c02a4b7482167bf923ce0d88fe47bed78015e22e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 17:47:31 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:31 up  6:24,  3 users,  load average: 1.77, 1.68, 1.32

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -19,7 +19,7 @@ static uint64_t get_time_internal(){
     
 }
 
-static uint64_t get_time(){
+uint64_t get_time(){
     if (boot_time == 0) boot_time = get_time_internal();
     uint64_t now = get_time_internal();
     return now - boot_time;
