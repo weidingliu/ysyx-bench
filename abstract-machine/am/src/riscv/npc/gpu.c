8ab549c0c0f8cc810b3de823971f2e1e65dd00c9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 15:31:33 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:31:33 up  4:08,  3 users,  load average: 0.54, 0.54, 0.44

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -10,8 +10,9 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t v1=inl(VGACTL_ADDR);
   printf("%x\n",VGACTL_ADDR);
+  uint32_t v1=inl(VGACTL_ADDR);
+  
   //uint32_t v2=inl(VGACTL_ADDR+4);
   /*uint64_t v=(v2<<32) +v1;
   printf("%016x\n",v);*/
