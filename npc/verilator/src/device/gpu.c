71ad3b21a93c030c6201a52a2a0285fbccce84d5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 15:15:12 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:12 up  3:51,  3 users,  load average: 0.45, 0.23, 0.26

--- npc/verilator/src/device/gpu.c
+++ npc/verilator/src/device/gpu.c
@@ -1,5 +1,5 @@
-#include <common.h>
-#include <device/map.h>
+#include <tb.h>
+#include <device.h>
 
 #define SCREEN_W 400
 #define SCREEN_H 300
@@ -16,8 +16,8 @@ static uint32_t screen_size() {
   return screen_width() * screen_height() * sizeof(uint32_t);
 }
 
-static void *vmem = NULL;
-static uint32_t *vgactl_port_base = NULL;
+void *vmem = malloc(screen_size());
+uint32_t *vgactl_port_base = (uint32_t *)malloc(8);
 
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #ifndef CONFIG_TARGET_AM
@@ -64,21 +64,19 @@ void vga_update_screen() {
       vgactl_port_base[1]=0;
   }
   
-  
-  
 }
 
 void init_vga() {
-  vgactl_port_base = (uint32_t *)new_space(8);
+ // vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
-#ifdef CONFIG_HAS_PORT_IO
+/*#ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
   add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
 #endif
 
   vmem = new_space(screen_size());
-  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
+  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);*/
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
---
+++ npc/verilator/src/device/mmio.c
@@ -0,0 +1,11 @@
+#include <tb.h>
+#include <device.h>
+
+void init_device(){
+    init_vga();
+}
+
+
+void write_device(long long addr, int len, long long data){
+
+}
