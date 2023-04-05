21b49ee85dc67b18dc37a7d8ca27db28ff4ccb03
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 17:11:08 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:08 up  5:47,  3 users,  load average: 0.46, 0.59, 0.60

--- npc/verilator/obj_dir/VCPUTop.mk
+++ npc/verilator/obj_dir/VCPUTop.mk
@@ -35,13 +35,12 @@ VM_PREFIX = VCPUTop
 VM_MODPREFIX = VCPUTop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-I /home/liuweiding/ysyx-workbench/npc/verilator/src/include -I /usr/lib/llvm-13/include -I /usr/include \
+	-I /home/liuweiding/ysyx-workbench/npc/verilator/src/include -I /usr/lib/llvm-13/include \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lLLVM-13 -ldl \
 	-lreadline \
-	-I /usr/include \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
