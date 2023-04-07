22f6a2a7ebc6372f2eb1fcd4df61346ddf52e221
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 14:14:00 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:14:00 up  1:53,  1 user,  load average: 0.68, 0.73, 0.83

--- npc/verilator/obj_dir/VCPUTop.mk
+++ npc/verilator/obj_dir/VCPUTop.mk
@@ -36,6 +36,7 @@ VM_MODPREFIX = VCPUTop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
 	-I /home/liuweiding/ysyx-workbench/npc/verilator/src/include -I /usr/lib/llvm-13/include \
+	-D WTRACE \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
