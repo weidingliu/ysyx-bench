e0b2612a08f7762cf0a6110f3df3d060ae05b466
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 18:39:05 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:39:05 up  6:49,  2 users,  load average: 0.91, 0.63, 0.43

--- npc/verilator/src/tb_CPUTop.cpp
+++ npc/verilator/src/tb_CPUTop.cpp
@@ -240,6 +240,7 @@ void Reset(VCoreTop *dut,VerilatedContext* contextp,VerilatedVcdC *m_trace){
     #ifdef DIFFTEST 
     memcpy(cpu.reg,cpu_gpr,sizeof(uint64_t)*32);
     cpu.pc=dut->io_pc;
+    printf("%x\n",cpu.pc);
     /*cpu.mepc=CSR[0];
     cpu.mcause=CSR[1];
     cpu.mstatus=CSR[2];
