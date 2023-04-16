703d4fbd72b3c11c9a7883eec0dd24ff61b72043
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 19:05:06 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:05:06 up  7:15,  2 users,  load average: 0.67, 1.02, 1.01

--- npc/src/main/scala/Pipline_CPU/backend/EXE.scala
+++ npc/src/main/scala/Pipline_CPU/backend/EXE.scala
@@ -312,7 +312,8 @@ class EXE extends Module with Paramete{
     is(ALUOPType.bltu) {
       dnpc := Mux(branch_flag === 1.U, branch_result, PC + 4.U(xlen.W))
     }
-    io.flush := Mux(branch_flag === 1.U || io.in.bits.ctrl_signal.fuType === FUType.jump,1.U,0.U)
+
+
 //    is(ALUOPType.ecall) {
 //      dnpc := csr_data
 //    }
@@ -329,7 +330,7 @@ class EXE extends Module with Paramete{
 //    csr.write(CSR_index.mstatus, csr.read(CSR_index.mstatus) & "hfffffffffffffff7".U(xlen.W))
 //  }
 
-
+  io.flush := Mux(branch_flag === 1.U || io.in.bits.ctrl_signal.fuType === FUType.jump,1.U,0.U)
   io.out.bits.ctrl_signal <> io.in.bits.ctrl_signal
   io.out.bits.ctrl_flow <> io.in.bits.ctrl_flow
   io.out.bits.ctrl_data <> io.in.bits.ctrl_data
