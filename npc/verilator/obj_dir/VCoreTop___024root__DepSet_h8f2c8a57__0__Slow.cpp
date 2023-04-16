6467b5d78f57d67f40e12300ec4d5a03a1d41e5d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 21:03:08 +0800

    > sim RTL ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:08 up  9:13,  2 users,  load average: 1.11, 0.75, 0.80

---
+++ npc/verilator/obj_dir/VCoreTop___024root__DepSet_h8f2c8a57__0__Slow.cpp
@@ -0,0 +1,180 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See VCoreTop.h for the primary calling header
+
+#include "verilated.h"
+#include "verilated_dpi.h"
+
+#include "VCoreTop___024root.h"
+
+VL_ATTR_COLD void VCoreTop___024root___initial__TOP__0(VCoreTop___024root* vlSelf);
+
+VL_ATTR_COLD void VCoreTop___024root___eval_initial(VCoreTop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VCoreTop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VCoreTop___024root___eval_initial\n"); );
+    // Body
+    vlSelf->__Vclklast__TOP__clock = vlSelf->clock;
+    VCoreTop___024root___initial__TOP__0(vlSelf);
+}
+
+VL_ATTR_COLD void VCoreTop___024root___settle__TOP__0(VCoreTop___024root* vlSelf);
+
+VL_ATTR_COLD void VCoreTop___024root___eval_settle(VCoreTop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VCoreTop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VCoreTop___024root___eval_settle\n"); );
+    // Body
+    VCoreTop___024root___settle__TOP__0(vlSelf);
+    vlSelf->__Vm_traceActivity[2U] = 1U;
+    vlSelf->__Vm_traceActivity[1U] = 1U;
+    vlSelf->__Vm_traceActivity[0U] = 1U;
+}
+
+VL_ATTR_COLD void VCoreTop___024root___final(VCoreTop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VCoreTop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VCoreTop___024root___final\n"); );
+}
+
+VL_ATTR_COLD void VCoreTop___024root___ctor_var_reset(VCoreTop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VCoreTop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VCoreTop___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clock = VL_RAND_RESET_I(1);
+    vlSelf->reset = VL_RAND_RESET_I(1);
+    vlSelf->io_pc = VL_RAND_RESET_Q(64);
+    vlSelf->io_inst = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__ID_io_out_bits_ctrl_signal_rfWen = VL_RAND_RESET_I(1);
+    for (int __Vi0=0; __Vi0<32; ++__Vi0) {
+        vlSelf->CoreTop__DOT__rf[__Vi0] = VL_RAND_RESET_Q(64);
+    }
+    vlSelf->CoreTop__DOT__mem_rdata = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__ID_io_in_bits_r_PC = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__ID_io_in_bits_r_Inst = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_src1Type = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_src2Type = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_fuType = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_inst_valid = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_rfWen = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_aluoptype = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_signal_rfDest = VL_RAND_RESET_I(5);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_data_src2 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_data_Imm = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_flow_PC = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX_io_in_bits_r_ctrl_flow_inst = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_signal_fuType = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_signal_inst_valid = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_signal_aluoptype = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_flow_PC = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_flow_inst = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_rf_rfDest = VL_RAND_RESET_I(5);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_rf_rfWen = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_rf_rfData = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_data_src1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_data_src2 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM_io_in_bits_r_ctrl_data_Imm = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_signal_inst_valid = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_flow_PC = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_flow_inst = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_rf_rfDest = VL_RAND_RESET_I(5);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_rf_rfWen = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__WB_io_in_bits_r_ctrl_rf_rfData = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_0_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_1_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_2_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_3_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_4_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_5_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_6_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_7_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_8_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_9_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_10_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_11_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_12_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_13_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_14_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_15_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_16_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_17_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_18_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_19_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_20_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_21_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_22_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_23_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_24_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_25_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_26_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_27_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_28_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_29_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_30_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_rf_31_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__DIP_io_inst_REG = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__DIP_io_inst_valid_REG = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__DIP_io_pc_REG = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__io_inst_REG = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__IF__DOT__temp = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__IF__DOT___temp_T_2 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__IFM__DOT__rdata = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_144 = VL_RAND_RESET_I(4);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_159 = VL_RAND_RESET_I(4);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_174 = VL_RAND_RESET_I(4);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_189 = VL_RAND_RESET_I(4);
+    vlSelf->CoreTop__DOT__ID__DOT__Inst_decode_0 = VL_RAND_RESET_I(4);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_209 = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_224 = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_239 = VL_RAND_RESET_I(3);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_268 = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_283 = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_298 = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_313 = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_316 = VL_RAND_RESET_I(7);
+    vlSelf->CoreTop__DOT__ID__DOT___Inst_decode_T_363 = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT__src1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_3 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT__src2 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_7 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_10 = VL_RAND_RESET_Q(64);
+    VL_RAND_RESET_W(128, vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_19);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_28 = VL_RAND_RESET_Q(33);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_38 = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_22 = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_58 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___alu_result_T_64 = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_14 = VL_RAND_RESET_Q(64);
+    VL_RAND_RESET_W(95, vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_5);
+    VL_RAND_RESET_W(95, vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_16);
+    vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_25 = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_28 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_31 = VL_RAND_RESET_I(32);
+    vlSelf->CoreTop__DOT__EX__DOT___shift_result_T_35 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_36 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___branch_result_T_1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__EX__DOT___GEN_51 = VL_RAND_RESET_I(1);
+    vlSelf->CoreTop__DOT__EX__DOT___dnpc_T_9 = VL_RAND_RESET_Q(64);
+    for (int __Vi0=0; __Vi0<32; ++__Vi0) {
+        vlSelf->CoreTop__DOT__DIP__DOT__rf[__Vi0] = VL_RAND_RESET_Q(64);
+    }
+    for (int __Vi0=0; __Vi0<4; ++__Vi0) {
+        vlSelf->CoreTop__DOT__DIP__DOT__IN[__Vi0] = VL_RAND_RESET_I(32);
+    }
+    vlSelf->CoreTop__DOT__MEM__DOT___addr_temp_T_1 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM__DOT___addr_temp_T_3 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM__DOT___GEN_16 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM__DOT__addr_temp = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM__DOT___wdata_temp_T_21 = VL_RAND_RESET_Q(64);
+    vlSelf->CoreTop__DOT__MEM__DOT___GEN_32 = VL_RAND_RESET_I(8);
+    vlSelf->CoreTop__DOT__MEM__DOT___mem_result_T_27 = VL_RAND_RESET_I(8);
+    vlSelf->CoreTop__DOT__MEM__DOT___mem_result_T_44 = VL_RAND_RESET_I(16);
+    vlSelf->__Vtask_pmem_read__0__rdata = 0;
+    vlSelf->__Vtask_pmem_read__4__rdata = 0;
+    for (int __Vi0=0; __Vi0<3; ++__Vi0) {
+        vlSelf->__Vm_traceActivity[__Vi0] = VL_RAND_RESET_I(1);
+    }
+}
---
+++ npc/verilator/obj_dir/VCoreTop___024root__Slow.cpp
@@ -0,0 +1,26 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See VCoreTop.h for the primary calling header
+
+#include "verilated.h"
+#include "verilated_dpi.h"
+
+#include "VCoreTop__Syms.h"
+#include "VCoreTop___024root.h"
+
+void VCoreTop___024root___ctor_var_reset(VCoreTop___024root* vlSelf);
+
+VCoreTop___024root::VCoreTop___024root(VCoreTop__Syms* symsp, const char* name)
+    : VerilatedModule{name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    VCoreTop___024root___ctor_var_reset(this);
+}
+
+void VCoreTop___024root::__Vconfigure(bool first) {
+    if (false && first) {}  // Prevent unused
+}
+
+VCoreTop___024root::~VCoreTop___024root() {
+}
