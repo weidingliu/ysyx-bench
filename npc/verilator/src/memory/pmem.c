cc546eded742d0bb570688cbbfc68e0486d164a6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 20:24:09 +0800

    > generate verilog ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:09 up 10:04,  3 users,  load average: 1.81, 1.87, 1.63

--- npc/verilator/src/memory/pmem.c
+++ npc/verilator/src/memory/pmem.c
@@ -48,6 +48,7 @@ extern "C" void pmem_read(long long addr, long long *rdata) {
 //     is_skip_ref=1;
 //     return;
 //   }
+printf("%016llx\n",(addr & ~0x7ull));
   if(addr>0x80000034){
       state=ABORT;
       return;
