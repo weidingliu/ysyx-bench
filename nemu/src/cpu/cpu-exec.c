83be6a92a3edb833b6437dbf60766a63ce408d7d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 19:46:13 +0800

    > compile NEMU ysyx_22020321 刘威丁 Linux liuweiding-Lenovo-Legion-Y7000 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:46:13 up  9:26,  3 users,  load average: 1.58, 1.67, 1.57

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -197,6 +197,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
 static void execute(uint64_t n) {
   Decode s;
+  printf("-----%x\n",n);
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     //printf("----------------%016lx\n",cpu.pc);
