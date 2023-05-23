945e49720cebce9c55a284659ee46e1e12f035dc
liuweidin <1045251744@qq.com>
2023-05-23 20:24:10 +0800

    On branch pa3  Changes to be committed: 	modified:   npc/src/main/scala/Pipline_CPU/axi/sram2axi.scala

--- npc/src/main/scala/Pipline_CPU/axi/sram2axi.scala
+++ npc/src/main/scala/Pipline_CPU/axi/sram2axi.scala
@@ -4,14 +4,45 @@ import chisel3._
 import chisel3.util._
 import firrtl.Utils.False
 
+import scala.collection.immutable
+
 class sram2axi extends Module with Paramete{
     val io = IO(new Bundle() {
       val in = new CPU_MEM_Bundle("WRIO")
       val out = new Axi_lite_Bundle_out
     })
-  val ar_valid = RegInit(false.B)
-  val aw_valid = RegInit(false.B)
-  val wd_valid = RegInit(false.B)
+  val idle :: read :: write :: Nil = Enum(3)
+  val state = RegInit(idle)
+  switch(state){
+    is(idle){
+      when(io.in.addr_req.valid && io.in.addr_req.bits.ce && !io.in.addr_req.bits.we){
+        state := read
+      }
+      when(io.in.addr_req.valid && io.in.addr_req.bits.ce && io.in.addr_req.bits.we) {
+        state := write
+      }
+    }
+    is(read){
+        when(io.in.rdata_rep.ready && io.out.rdata_rep.valid){
+          state := idle
+        }
+    }
+    is(write){
+        when(io.out.wb.valid){
+          state := idle
+        }
+    }
+  }
 
+  io.in.addr_req.ready := Mux(state === idle ,1.B,0.B)
+  io.in.wdata_req.get.ready := Mux(state === idle ,1.B,0.B)
 
+  io.in.rdata_rep.valid := Mux(state === read && io.out.rdata_rep.valid,1.B,0.B)
+
+  io.out.rdata_rep.ready := io.in.rdata_rep.ready
+  io.out.wb.ready := Mux(state === write,1.B,0.B )
+
+  io.out.raddr_req.valid := Mux(state === idle && io.in.addr_req.valid && io.in.addr_req.bits.ce && !io.in.addr_req.bits.we,1.B,0.B)
+  io.out.waddr_req.valid := Mux(state === idle && io.in.addr_req.valid && io.in.addr_req.bits.ce &&  io.in.addr_req.bits.we,1.B,0.B)
+  io.out.wdata_req.valid := Mux(state === idle && io.in.addr_req.valid && io.in.addr_req.bits.ce &&  io.in.addr_req.bits.we,1.B,0.B)
 }
