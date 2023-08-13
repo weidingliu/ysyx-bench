b10334106ce6ca6941e6da8371edcc840dd606aa
liuweidin <1045251744@qq.com>
2023-08-13 20:53:47 +0800

    On branch pa3  Changes to be committed: 	new file:   npc/src/main/scala/Pipline_CPU/backend/div.v

---
+++ npc/src/main/scala/Pipline_CPU/backend/div.v
@@ -0,0 +1,22 @@
+
+
+module div(
+    input wire clock,
+    input wire reset,
+
+    input wire in_valid,
+    input wire [63:0] in_a,
+    input wire [63:0] in_b,
+    input wire div_signed,
+
+    output wire result_valid,
+    output wire [63:0]quotient,
+    output wire [63:0]remainder
+);
+    
+
+
+endmodule
+
+
+
