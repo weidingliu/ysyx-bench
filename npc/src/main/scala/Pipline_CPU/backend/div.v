16370ff20682fd1a439e6b0e0247380201887287
liuweidin <1045251744@qq.com>
2023-08-15 22:23:08 +0800

    On branch pa3  Changes to be committed: 	modified:   npc/src/main/scala/Pipline_CPU/backend/div.v

--- npc/src/main/scala/Pipline_CPU/backend/div.v
+++ npc/src/main/scala/Pipline_CPU/backend/div.v
@@ -17,7 +17,7 @@ module div(
 
 parameter idle = 2'b00;
 parameter Run = 2'b01;
-parameter End = 2'b10;
+parameter eNDs = 2'b10;
 
 reg [127:0]dividend;
 reg [63:0]divisor;
@@ -34,9 +34,16 @@ wire [63:0]negtive_R;
 wire [63:0]negtive_a;
 wire [63:0]negtive_b;
 
+wire [63:0] s_o;
+wire [63:0] r_o;
+
+wire [63:0]res_div;
+
+assign R = dividend[127:64];
+assign res_div = dividend[127:63] - {1'b0,divisor};
 assign s = count == 9'd64;
 assign negtive_S = ~S + 'h1;
-assign negtive_R = ~dividend[127:64] + 'h1;
+assign negtive_R = ~R + 'h1;
 
 assign negtive_a = ~in_a + 'h1;
 assign negtive_b = ~in_b + 'h1;
@@ -51,9 +58,9 @@ always @(posedge clk) begin
                 if(in_valid) state <= Run;
             end
             Run: begin
-                if(s) state <= End;
+                if(s) state <= eNDs;
             end
-            End: begin
+            eNDs: begin
                 state <= idle;
             end
             default: state <= idle;
@@ -83,14 +90,33 @@ always @(posedge clk) begin
                 divisor <= (div_signed & in_b[63]) ? negtive_b:in_b;
             end
             Run: begin
-
+                S <= res_div[64] ? {S[62:0],1'b0}:{S[62:0],1'b1};
+                dividend <= (res_div[64]? {res_div,dividend[62:0]}: dividend) << 1;
             end
-            End: begin
+            eNDs: begin
 
             end
         endcase
     end
 end
+// counter 
+always @(posedge clk) begin
+    if(reset) begin 
+        count <= 9'b0;
+    end
+    else begin 
+        if(state == Run) count <= count + 9'b1;
+        if(state == idle) count <= 9'b0;
+    end
+end
+
+assign s_o = (in_a[63] ^ in_b[63])? ({64{S[63]}} & S | {64{!S[63]}} & negtive_S):({64{S[63]}} & negtive_S | {64{!S[63]}} & S);
+assign r_o = (in_a[63]) ? ({64{R[63]}} & R | {64{!R[63]}} & negtive_R):({64{R[63]}} & negtive_R | {64{!R[63]}} & R);
+
+assign result_valid = state == eNDs;
+assign quotient = {64{div_signed}} & s_o | {64{!div_signed}} & S;
+assign remainder = {64{div_signed}} & r_o | {64{!div_signed}} & R;
+
 
 endmodule
 
